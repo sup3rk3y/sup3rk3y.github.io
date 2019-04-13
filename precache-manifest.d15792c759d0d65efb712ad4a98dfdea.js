11829bdb038b5158bf14114e3b33045a29b4f2f3
sup3rk3y <lalosapost@gmail.com>
2019-04-13 23:04:55 +0200

    initial commit

---
+++ precache-manifest.d15792c759d0d65efb712ad4a98dfdea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ebdd55723df592adc8a3",
+    "url": "/static/js/main.b3b5f122.chunk.js"
+  },
+  {
+    "revision": "85c702a8a4432e0a4828",
+    "url": "/static/js/2.eb2a54f2.chunk.js"
+  },
+  {
+    "revision": "ebdd55723df592adc8a3",
+    "url": "/static/css/main.060dc23d.chunk.css"
+  },
+  {
+    "revision": "fed084cdb1a4d8bfb352435382566d37",
+    "url": "/index.html"
+  }
+];
