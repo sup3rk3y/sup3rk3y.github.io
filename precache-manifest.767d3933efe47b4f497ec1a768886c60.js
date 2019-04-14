fded9f2cdea3e47f67d266a4d0bf1f00bd068e26
sup3rk3y <lalosapost@gmail.com>
2019-04-14 16:15:46 +0200

    - fixed some text and added css

---
+++ precache-manifest.767d3933efe47b4f497ec1a768886c60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "63e5f0becfb5cffb986772e0ad4c9c16",
+    "url": "/static/media/image.63e5f0be.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "480ffb1b61d0518a2cb8",
+    "url": "/static/js/main.2abbd789.chunk.js"
+  },
+  {
+    "revision": "8336cdd236512b926524",
+    "url": "/static/js/2.ce785e65.chunk.js"
+  },
+  {
+    "revision": "480ffb1b61d0518a2cb8",
+    "url": "/static/css/main.5dd36257.chunk.css"
+  },
+  {
+    "revision": "9545fa8f6cad556a3f5d455026eaf105",
+    "url": "/index.html"
+  }
+];
